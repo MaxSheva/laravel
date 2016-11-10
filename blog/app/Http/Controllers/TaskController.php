95e1d38db8111f8863d4dfb1d9dd2b641a4935c9
Max <Max@Max>
2016-11-10 20:02:35 +0200

    edit

---
+++ blog/app/Http/Controllers/TaskController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class TaskController extends Controller {
+
+    /**
+     * Создание нового экземпляра контроллера.
+     *
+     * @return void
+     */
+    public function __construct() {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Отображение списка всех задач пользователя.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function index(Request $request) {
+        $tasks = Task::where('user_id', $request->user()->id)->get();
+
+        return view('tasks.index', [
+            'tasks' => $tasks,
+        ]);
+    }
+
+    /**
+     * Create a new task.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request) {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+        ]);
+        $request->user()->tasks()->create([
+            'name' => $request->name,
+        ]);
+        return redirect('/tasks');
+    }
+
+}
